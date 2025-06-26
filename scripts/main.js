ac085cb749e58ebbb653d05833414aecc098f28a
Alan D. E. Cruse <alancruse@tutanota.com>
2025-06-26 09:12:05 +0200

    Create browser langauge detection logic

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,22 @@
-const userLang = navigator.language;
+console.log("Wedding page loaded");
+const userLang = navigator.language.slice(0, 2);
+console.log(userLang);
 
+langEn = document.querySelectorAll("[data-lang='en']");
+langDe = document.querySelectorAll("[data-lang='de']");
+console.log(langEn);
+console.log(langDe);
+
+function setContentLanguage(params) {
+    if (userLang == "en") {
+        langDe.forEach(element => {
+            element.style.display = "none";
+        });
+    } else {
+        langEn.forEach(element => {
+            element.style.display = "none";
+        })
+    }
+}
+
+setContentLanguage();
